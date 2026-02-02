53807e3f4a5f6fd9346425ca8879a79b7c0bd928
rdme0 <71927381+rdme0@users.noreply.github.com>
2026-02-02 19:46:45 +0900

    :recycle: refactor: SpotifySecurityProperties 하드코딩 제거 및 yml 설정으로 외부화

--- src/main/kotlin/backend/spotify/config/SpotifyConfig.kt
+++ src/main/kotlin/backend/spotify/config/SpotifyConfig.kt
@@ -5,9 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "spotify.api")
 data class SpotifyApiProperties(val baseUrl: String)
 
-@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.spotify")
+@ConfigurationProperties(prefix = "spotify.security")
 data class SpotifySecurityProperties(
-    val clientId: String,
-    val clientSecret: String,
-    val tokenUrl: String = "https://accounts.spotify.com/api/token"
+        val tokenUrl: String,
+        val clientId: String,
+        val clientSecret: String
 )
