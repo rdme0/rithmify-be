ede07c6779beab29ab0328881f10ea41a1b2a4bc
rdme0 <71927381+rdme0@users.noreply.github.com>
2026-02-03 13:42:07 +0900

    :wrench: chore: Spring Data Web 페이지네이션 직렬화 설정 추가 (VIA_DTO)

--- src/main/kotlin/backend/TierifyBeApplication.kt
+++ src/main/kotlin/backend/TierifyBeApplication.kt
@@ -3,9 +3,13 @@ package backend
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.web.config.EnableSpringDataWebSupport
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 class TierifyBeApplication
 
 fun main(args: Array<String>) {
